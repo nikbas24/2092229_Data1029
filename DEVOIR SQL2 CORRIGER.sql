ddc721bfc60d1f5f0f4789f2311a644fde1a482f
Nikolas Basque <157416150+nikbas24@users.noreply.github.com>
2024-01-30 09:05:32 -0400

    Add files via upload

---
+++ DEVOIR SQL2 CORRIGER.sql
@@ -0,0 +1,7 @@
+SELECT title, price FROM `titles` WHERE title LIKE '%computer%';
+SELECT title, price FROM titles WHERE title LIKE '%computer%' AND title NOT LIKE '%computer%' ;
+SELECT title, price FROM titles where titles.type rlike '[sb]u%';
+SELECT title, price FROM titles WHERE titles.type NOT LIKE 'SU%' AND titles.type NOT LIKE 'BU%';
+SELECT title, price FROM titles WHERE (SUBSTRING(title, 1, 1) NOT LIKE 'S' AND SUBSTRING(title, 1, 1) NOT LIKE 'B') AND SUBSTRING(title, 2, 1) = 'o';
+SELECT title, price FROM titles WHERE (SUBSTRING(title, 1, 1) NOT LIKE 'S' AND SUBSTRING(title, 1, 1) NOT LIKE 'B') AND SUBSTRING(title, 3, 1) = 'f';
+SELECT title, price FROM titles WHERE SUBSTRING(title, 1, 1) IN ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
